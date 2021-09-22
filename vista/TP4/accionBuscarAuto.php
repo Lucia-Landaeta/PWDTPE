649999413464cd7ab91afea1f671bf3c5722aec4
Lucia-Landaeta <lucia.landaeta@est.fi.uncoma.edu.ar>
2021-09-22 11:36:59 -0300

    modificacion

--- vista/TP4/accionBuscarAuto.php
+++ vista/TP4/accionBuscarAuto.php
@@ -21,8 +21,7 @@ if (isset($datos['patente'])) {
                     <thead class="table-secondary">
                         <tr><th colspan="4" class="text-center"><h5>Información Auto</h5> </th></tr>
                         <?php
-                        if($objAuto!=null){
-                        echo"
+                        if($objAuto!=null){ ?>
                         <tr>
                             <th scope='col'>Patente</th>
                             <th scope='col'>Marca</th>
@@ -32,20 +31,18 @@ if (isset($datos['patente'])) {
                     </thead>
                     <tbody>
                         <tr>
-                            <td>". $objAuto->getPatente()."</td>
-                            <td>".$objAuto->getMarca()."</td>
-                            <td>".$objAuto->getModelo()."</td>
-                            <td>".$objAuto->getObjDuenio()->getNroDni()."</td>
+                            <td> <?php echo $objAuto->getPatente() ?> </td>
+                            <td> <?php echo $objAuto->getMarca() ?> </td>
+                            <td> <?php echo $objAuto->getModelo() ?> </td>
+                            <td> <?php echo $objAuto->getObjDuenio()->getNroDni() ?> </td>
                         </tr>
                     </tbody>
-                    ";
-                        }else{
-                            echo"
+                    <?php
+                        }else{ ?>
                             <tr>
-                                <td colspan='4' scope='col'>El auto con patente ".$datos['patente']." no se encuentra cargado.</td>
+                                <td colspan='4' scope='col'>El auto con patente <?php echo $datos['patente'] ?> no se encuentra cargado.</td>
                             </tr>
-                            ";
-                        }
+                 <?php }
                     ?>
                 </table>
                 <a href="buscarAuto.php"><button type="button" class="btn btn-outline-primary mt-3">Volver</button>
